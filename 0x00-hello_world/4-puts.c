9fa168892436d076a96fafa7d9ca6f03c749893e
Brightlink01 <Brightlink01@gmail.com>
2022-04-07 07:54:31 -0700

    the style message

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
-#!/bin/bash
 #include <stdio.h>
 
 /**
-* main -code entry point
+* main - Eentry point
 * 
 * Return: always 0 (successful)
 */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
