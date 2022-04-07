fefd209598a23a78b8dab7c40f3954e2c58b8f11
Brightlink01 <Brightlink01@gmail.com>
2022-04-07 07:07:33 -0700

    betty coding style edited

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- *main - mmain block
- *return (0);
- */
+ * * main - entry point
+ *  * 
+ *   * :Return always 0 (successful)
+ *    */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+               puts("\"Programming is like building a multilingual puzzle");
+                            return (0);
 }
