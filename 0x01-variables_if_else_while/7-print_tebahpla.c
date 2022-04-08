097a3d930405940c9989dc444392a6ce63acf7bf
Brightlink01 <Brightlink01@gmail.com>
2022-04-08 14:08:23 -0700

    commit the reversee

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entery point
+ *
+ * Return: Always 0 (success/correct)
+ */
+
+int main(void)
+{
+
+char alp;
+
+for (alp = 'z'; alp >= 'a'; alp--)
+{
+putchar(alp);
+}
+
+putchar('\n');
+
+return (0);
+}
