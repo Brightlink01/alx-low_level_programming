e631eef427e3a648fc5da0f31ff4131a19c027b6
Brightlink01 <Brightlink01@gmail.com>
2022-04-08 07:53:53 -0700

    commit message

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,7 +13,7 @@ int main(void)
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
- if (n == 0)
+if (n == 0)
 {
 printf("%i is zero\n", n);
 }
@@ -26,5 +26,5 @@ else
 printf("%i is positive\n", n);
 }
 
-return(0);
+return (0);
 }
