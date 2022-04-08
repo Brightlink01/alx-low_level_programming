1e2c9ecbf80ce67e2add6ed6b7e23c5aa13743ca
Brightlink01 <Brightlink01@gmail.com>
2022-04-08 11:49:34 -0700

    alphabet print

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success/correct)
+ */
+int main(void)
+{
+char alph;
+for (alph = 'a'; alph <= 'Z'; alph++)
+{
+putchar(alph);
+}
+putchar('\n');
+
+return (0);
+}
