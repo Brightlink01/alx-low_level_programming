ed47448769352563c3e7ecf33bd984494b5c635a
Brightlink01 <Brightlink01@gmail.com>
2022-04-08 12:29:42 -0700

    commit message

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entery point
+ *
+ * Return: Always 0 (success/correct)
+ */
+
+int main(void)
+{
+
+char alp;
+
+for (alp = 'a'; alp <= 'z'; alp++)
+{
+if (alp == 'e')
+{
+;
+}
+else if(alp == 'q')
+{
+;
+}
+else
+{
+putchar(alp);
+}
+
+putchar('\n');
+
+return (0);
+}
