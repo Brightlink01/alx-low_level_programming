6528ad71c69b1c9500b1f44b94294a4d91a4e948
Brightlink01 <Brightlink01@gmail.com>
2022-04-08 08:33:28 -0700

    commit message

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,8 @@ if (n % 10 > 5)
 {
 printf("Last digit of %i is %i and is greater than 5\n", n, n % 10);
 }
-else if ("Last digit of %i is %i is zero\n", n, n % 10);
+else if (n % 10 == 0)
+printf("Last digit of %i is %i is zero\n", n, n % 10);
 {
 printf("Last digit of %i is %i and is less than 6 and not zero\n", n, n % 10);
 }
