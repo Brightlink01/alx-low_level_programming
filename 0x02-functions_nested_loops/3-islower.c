cc9cb3025856917eba605022f88af8af21e42f8a
Brightlink01 <Brightlink01@gmail.com>
2022-04-12 10:57:08 -0700

    commit the tas 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include"main.h"
+
+/**
+* int _islower - is a function check for lower or upper
+* returns:  1 or 0
+*/
+
+int _islower(int c)
+{
+if (c <= 'a' && c <= 'z')
+return (1);
+else
+return (0);
+}
