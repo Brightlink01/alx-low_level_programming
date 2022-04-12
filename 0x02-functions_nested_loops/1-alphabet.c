2bf9bbdd4b41ab73c6440f4aa6602a98422016a1
Brightlink01 <Brightlink01@gmail.com>
2022-04-12 10:12:51 -0700

    task 1 of level programing

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * lower case alphabet prting functions
+ * the it done using for looping 
+ *
+ */
+
+void print_alphabet(void)
+{
+char alp;
+
+for (alp = 'a'; alp <= 'z'; alp++)
+{
+
+_putchar(alp);
+
+_putchar('\n');
+}
+}
+
+/**
+ * main - check the code
+ * Return: Always 0
+ */
+
+int main(void)
+{
+
+print_alphabet();
+
+return (0);
+
+}
