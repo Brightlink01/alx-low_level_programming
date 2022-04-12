bf0836b2de583f8bf881d8a321c25b0b2a8bc68e
Brightlink01 <Brightlink01@gmail.com>
2022-04-12 11:12:30 -0700

    betty documetations

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,6 +3,7 @@
 /**
 * _islower - take int parametr checks for lower or upper
 * returns: 1 for lower case and return 0 for upper case Alphabet
+* c: parameter takes in character
 */
 
 int _islower(int c)
