480ea92bf4502efceae63ea4537477c2fa3142a4
Brightlink01 <Brightlink01@gmail.com>
2022-04-12 15:42:11 -0700

    commiting funky

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,24 +15,26 @@ for (i = 0; i <= 9; i++)
 for (j = 0; j <= 9; j++)
 {
 k = i * j;
-if (j == 0)
-{
-_putchar(k + '0');
-}
-else if (k <= 9)
+if (j != 0)
 {
 _putchar(',');
 _putchar(' ');
-_putchar(k / 10 + '0');
 }
-else if (k >= 10)
+if (k >= 10)
 {
-_putchar(',');
+_putchar((k / 10) + '0');
+_putchar((k % 10) + '0');
+}
+else if (k < 10 && j != 0)
+{
 _putchar(' ');
-_putchar(k / 10 + '0');
 _putchar(k % 10 + '0');
 }
+else
+{
+_putchar((k % 10) + '0');
 }
 _putchar('\n');
+}
 }
 }
