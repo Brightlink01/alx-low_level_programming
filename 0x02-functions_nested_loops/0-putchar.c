ec9491a86d57ab107bd366a21513a3f6a3249cb9
Brightlink01 <Brightlink01@gmail.com>
2022-04-12 07:10:49 -0700

    the task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include"main.h"
+
+/**
+ * main - entry func
+ * Return: 0 always 
+ */
+
+int main(void)
+{
+char _putc[] = "_putchar";
+int c;
+
+for (c = 0; c <= 8; c++)
+{
+_putchar(_putc[c]);
+}
+_putchar('\n');
+return (0);
+}
