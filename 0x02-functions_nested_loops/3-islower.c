9c0bfd457ea35d71d355ecf0593e5fb04b35ea18
Brightlink01 <Brightlink01@gmail.com>
2022-04-12 11:23:13 -0700

    commit meassge

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,9 @@
 #include"main.h"
 
 /**
-* _islower - take int parametr checks for lower or upper
-* @c: takes character 
-* Returns: 1 for lower case, 0 for upper case Alphabet
-*
+*_islower - returns 1 if lower case and 0 otherwise
+*@c: takes character
+*Returns: 1 for lower case, 0 for upper case Alphabet
 */
 
 int _islower(int c)
