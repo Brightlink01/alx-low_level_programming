b8ba40c148f73b38ac53c6493472f20ab3e27f01
Brightlink01 <Brightlink01@gmail.com>
2022-04-12 15:16:47 -0700

    commiting task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,32 @@
+#include"main.h"
+
+/**
+ *print_to_98 - return nothing
+ *@n: Takes in an integer
+ */
+
+void print_to_98(int n)
+{
+int i;
+if ( n > 98)
+for (i = n; i > 97; i--)
+{
+printf("%d", i);
+if (i != 98)
+{
+printf(", ")
+}
+}
+else
+{
+for ( i = n; i < 99; i++)
+{
+printf("%d", i);
+if (i != 98)
+{
+printf(", ");
+}
+}
+}
+printf("\n");
+}
