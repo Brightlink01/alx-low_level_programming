b99d7e55c26b0f8f944b3153105120d7f307e717
Brightlink01 <Brightlink01@gmail.com>
2022-04-12 15:24:21 -0700

    commiting messages

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,7 +9,7 @@
 void print_to_98(int n)
 {
 int i;
-if ( n > 98)
+if (n > 98)
 for (i = n; i > 97; i--)
 {
 printf("%d", i);
@@ -20,7 +20,7 @@ printf(", ");
 }
 else
 {
-for ( i = n; i < 99; i++)
+for (i = n; i < 99; i++)
 {
 printf("%d", i);
 if (i != 98)
