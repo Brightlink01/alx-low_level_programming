6880d96e673be77ec0b9d7c594e41befca595a3e
Brightlink01 <Brightlink01@gmail.com>
2022-04-12 12:15:29 -0700

    sign identification

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include"main.h"
+
+/**
+ *print_sign - takes intiger and return 1 for posative 0 for zero and 
+-1 for negative n value
+*@n: takes integer value
+*Return: 1 for psitive number, 0 for zero and -1 for negative number
+*/
+
+int print_sign(int n)
+{
+if (n > 0)
+{
+return (1);
+_putchar('+');
+}
+else if (n < 0 )
+{
+_putchar('-');
+return (-1);
+}
+else
+{
+_putchar('0');
+return (0);
+}
+}
