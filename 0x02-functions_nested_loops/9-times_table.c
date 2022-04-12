fb0c1617371bbb013b72b04723183d40101d6c82
Brightlink01 <Brightlink01@gmail.com>
2022-04-12 14:34:25 -0700

    commited message

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,7 +15,7 @@ for (i = 0; i <= 9; i++)
 for (j = 0; j <= 9; j++)
 {
 k = i * j;
-if (j==0)
+if (j == 0)
 {
 _putchar(k + '0');
 }
@@ -25,7 +25,7 @@ _putchar(',');
 _putchar(' ');
 _putchar(k / 10 + '0');
 }
-else if (k >= 9)
+else if (k >= 10)
 {
 _putchar(',');
 _putchar(' ');
