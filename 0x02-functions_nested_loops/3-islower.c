426577586cb4ffe785cc8a527919e3d804044f45
Brightlink01 <Brightlink01@gmail.com>
2022-04-12 11:32:32 -0700

    lower and upper case

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,9 @@
 #include"main.h"
 
 /**
-*_islower - returns 1 if lower case alphabet and 0 otherwise
+*_islower - return 1 if lower case alphabet and 0 otherwise
 *@c: takes character
-*Returns: 1 for lower case, 0 for upper case Alphabet
+*Return: 1 for lower case, 0 for upper case Alphabet
 */
 
 int _islower(int c)
