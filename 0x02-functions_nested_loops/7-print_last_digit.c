452d608b63f7f1c45a5cd29b0c32d5ff03144108
Brightlink01 <Brightlink01@gmail.com>
2022-04-12 13:26:06 -0700

    commiting message

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include"main.h"
+
+/**
+ *print_last_digit - prints last digit of number
+ *@n:takes an intiger 
+ *Return: Return output
+ *
+ */
+int print_last_digit(int n)
+{
+int ln;
+ln = n % 10;
+if (n < 0)
+ln = ln * -1;
+_putchar(ln + '0');
+
+return (0);
+}
+  
+  
