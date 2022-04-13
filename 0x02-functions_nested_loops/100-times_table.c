6f2607a7d995cb50b2682f391b47df474440b266
Brightlink01 <Brightlink01@gmail.com>
2022-04-13 11:17:04 -0700

    commitng message

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,7 +10,7 @@ void print_times_table(int n)
 int i;
 int j;
 int k;
-if (n <= 15 || n >= 0)
+if (n <= 15 && n >= 0)
 {
 for (i = 0; i <= n; i++)
 {
@@ -21,14 +21,23 @@ if (j != 0)
 {
 _putchar(',');
 _putchar(' ');
+_putchar(' ');
 }
 if (k >= 10)
 {
+_putchar(' ');
 _putchar((k / 10) + '0');
 _putchar((k % 10) + '0');
 }
+else if (k >= 100)
+{
+_putchar(( k / 100)) + '0');
+_putchar(((k % 100) / 10) + '0');
+_putchar(((k % 100) % 10) + '0');
+}
 else if (k < 10 && j != 0)
 {
+_putchar(' ');
 _putchar(' ');
 _putchar(k % 10 + '0');
 }
