caef8375ea28b0ef402e934376cf23612b0fefdd
Brightlink01 <Brightlink01@gmail.com>
2022-04-13 12:40:12 -0700

    commiting the message

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -18,35 +18,33 @@ for (i = 0; i <= n; i++)
 for (j = 0; j <= n; j++)
 {
 k = i * j;
-if (j != 0)
+if (k < 10 && j != 0)
 {
-_putchar(',');
 _putchar(' ');
 _putchar(' ');
+_putchar(' ');
+_putchar(k + '0');
+(k >= 100)
 }
-if (k >= 10)
+else if (k < 100)
 {
+_putchar(' ');
 _putchar(' ');
 _putchar((k / 10) + '0');
 _putchar((k % 10) + '0');
 }
-else if (k >= 100)
+else if (k < 1000)
 {
+_putchar(' ');
 _putchar((k / 100) + '0');
 _putchar(((k % 100) / 10) + '0');
 _putchar(((k % 100) % 10) + '0');
 }
-else if (k < 10 && j != 0)
+if (j != n)
 {
-_putchar(k % 10 + '0');
-}
-else
-{
-_putchar(' ');
-_putchar((k % 10) + '0');
+_putchar(',');
 }
 }
 _putchar('\n');
-}
 }
 }
