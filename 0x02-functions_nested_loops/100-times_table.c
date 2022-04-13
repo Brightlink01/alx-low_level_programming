fa0d5c9af38230f3a67927deafe0fedd247d17c5
Brightlink01 <Brightlink01@gmail.com>
2022-04-13 12:49:17 -0700

    commitng the message

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -23,7 +23,7 @@ if (k < 10 && j != 0)
 _putchar(' ');
 _putchar(' ');
 _putchar(' ');
- _putchar(k + '0');
+_putchar(k + '0');
 }
 else if (k < 100)
 {
@@ -44,7 +44,7 @@ if (j != n)
 _putchar(',');
 }
 }
-}
 _putchar('\n');
+}
 }
 }
