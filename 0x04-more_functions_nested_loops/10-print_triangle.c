796906f81b4efcc5a7a29a081ff0c36d1e46717c
Brightlink01 <Brightlink01@gmail.com>
2022-04-14 12:01:35 -0700

    commiting the task

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
 *print_triangle - prints a triangle, followed by a new line
 *@size: size of the triangle
@@ -20,6 +21,7 @@ for (j = i; j < size; j++)
 _putchar(' ');
 }
 for (j = 1; j <= i; j)
+{
 _putchar('#');
 }
 _putchar('\n');
