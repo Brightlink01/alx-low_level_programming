42db888d8e55cb42037a5e4d5e566ceea7c25a3b
Brightlink01 <Brightlink01@gmail.com>
2022-04-15 13:58:14 -0700

    commiting the task 1 edited

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -3,13 +3,15 @@
 /**
 * main - causes an infinite loop
 * Return: 0 always
+*"\\o/\n": is used to diagola printing
 */
 
 int main(void)
 {
 int i;
 printf("Infinite loop incoming :(\n");
-i = 0;
+
+ i = 0;
 /**
 * while (i < 10)
 * {
@@ -18,5 +20,5 @@ i = 0;
 */
 printf("Infinite loop avoided! \\o/\n");
 
-return (0);
+ return (0);
 }
