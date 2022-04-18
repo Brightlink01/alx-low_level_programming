8a1c64d89a812d0c2b31a07b0eea08636337386d
Brightlink01 <Brightlink01@gmail.com>
2022-04-18 11:23:47 -0700

    my commits on pointers task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include"main.h"
+
+/**
+ *void reset_to_98 - update the n value
+ *@n: is the pointer
+ *Return: void that means correct
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
+ 
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+the 0x05.c pointer and arrays_string manipulations
