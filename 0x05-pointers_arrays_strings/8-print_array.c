b1b540f4b939a8e78327af8dac2885d151485356
Brightlink01 <Brightlink01@gmail.com>
2022-04-18 14:05:35 -0700

    commiting task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include"main.h"
+#include<stdio.h>
+
+/**
+ *print_array - array pringing func
+ *@a: pointer of int type
+ *@n: inegral variable
+ *
+ *Return: void means successful
+ *
+ */
+
+void print_array(int *a, int n)
+{
+int i;
+while (i < n)
+{
+printf("%d", [i]);
+if (i < n-1)
+{
+printf(", ");
+}
+i++;
+}
+printf("\n");
+}
