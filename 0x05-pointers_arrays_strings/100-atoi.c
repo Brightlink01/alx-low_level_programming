17012416b8afaffe84cd053739c6103d10e9b7c5
Brightlink01 <Brightlink01@gmail.com>
2022-04-18 15:20:51 -0700

    my commit of 100

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,44 @@
+#include"main.h"
+/**
+ *is_numberical - check if it is a digit
+ *@n: number 
+ *Return: if it is number 1 other wise 0
+ */
+
+int is_numerical(unsigned int n)
+{
+return (n >= '0' && n <= '9');
+}
+
+/**
+ *_atoi - willing to sacrfices nuber than people
+ *@s: pointer value 
+ *
+ *Return: integeral type
+ *
+ */
+
+int _atoi(char *s)
+{
+unsigned int num, i;
+int sign;
+
+sign = 1;
+num = 0;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+if (is_numerical(s[i])
+{
+num = (s[i] - 48) + nm * 10;
+
+if (s[i + 1] == ' ')
+break;
+}
+else if (s[i] == '-')
+{
+sing *= -1;
+}
+return (num * sing);
+}
+  
