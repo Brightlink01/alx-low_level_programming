f3903aa656de892fd451bc4fd09898923a0b7695
Brightlink01 <Brightlink01@gmail.com>
2022-04-18 11:59:01 -0700

    commitng the answer

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,17 +1,17 @@
 #include"main.h"
 
 /**
- *_strlen - length of the string
- *@s: A pointer to an int 
- *Return: void that means always corrct
- *
- */
+*_strlen - length of the string
+*@s: A pointer to an int
+*Return: void that means always corrct
+*
+*/
 
 int _strlen(char *s)
 {
 int u;
 u = 0;
-while(s[u] != '\0')
+while (s[u] != '\0')
 {
 u++;
 }
