8c44ea527b654c446a6016c31fb2e4b488a3e93b
Brightlink01 <Brightlink01@gmail.com>
2022-04-20 14:05:05 -0700

    commitng the assingment task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include"main.h"
+
+/**
+ *_strncpy -the functionction copy the string
+ *@dest: the destination pointer hold copy
+ *@src: the pointerholds thestring to be copied
+ *@n: the n neber strings has to be copied
+ *Return: Returns dest;
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+i = 0;
+while (i < n && src[i] != '\0')
+{
+dest[i] = src[i];
+i++;
+}
+dest[i] = '\0';
+}
