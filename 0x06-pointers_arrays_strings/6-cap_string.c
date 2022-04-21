681c5189d7736091217d77fe0d395f4c2e758bbc
Brightlink01 <Brightlink01@gmail.com>
2022-04-21 14:07:30 -0700

    commitng the worrest case

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,4 @@
 #include"main.h"
-#include <ctype.h>
 
 /**
  *cap_string - it capitalizes all words of a string
@@ -8,13 +7,14 @@
  */
 char *cap_string(char *str)
 {
-char sy[] = ",;.!?(){  \"}";
+char sy[] = ",;.\t!?(){  \"}";
 int i, j;
 int count;
 for (i = 0; str[i] != '\0'; i++)
 {
-if (str[0] > 96 && str[0] < 123)
+if (str[0] > 'a' && str[0] < 'z')
 count = 1;
+{
 for (j = 0; sy[j] != '\0'; j++)
 {
 if (sy[j] == str[i])
@@ -22,12 +22,13 @@ count = 1;
 }
 if (count)
 {
-if (str[i] > 96 && str[i] < 123)
+if (str[i] > 'a' && str[i] < 'z')
 {
 str[i] -= 32;
 count = 0;
 }
-else if (str[i] > 6 && str[i] < 91)
+}
+else if (str[i] > 64 && str[i] < 91)
 count = 0;
 else if (str[i] > 47 && str[i] < 58)
 count = 0;
