4958021bdb8ddfc591341c5e20d28539fba71953
Brightlink01 <Brightlink01@gmail.com>
2022-04-21 08:48:15 -0700

    commiting the task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,10 +9,11 @@
 
 int _strcmp(char *s1, char *s2)
 {
-while (*s1 && (*s1 == *s2))
+int i;
+for (i = 0; s[i] !='\0' || s2[i] != '\0'; i++)
 {
-s1++;
-s2++;
+if (s1[i] != s2[i])
+return (*s1 - *s2);
 }
-return *s1 - *s2;
+return(0);
 }
---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,12 @@
+#include"main.h"
+
+/**
+ *cap_string - it capitalizes all words of a string
+ *@str: pointer typed char and has string value
+ *Return: char str
+ */
+char *cap_string(char *str)
+{
+  char sy[] = ",;.!?(){  \"}";
+
+}
