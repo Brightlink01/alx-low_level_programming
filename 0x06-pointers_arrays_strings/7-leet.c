9597bbd3b2e8170ab0e64aec17372fd47cdb706d
Brightlink01 <Brightlink01@gmail.com>
2022-04-21 11:59:27 -0700

    committing task 7 have no time

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -16,11 +16,14 @@ char num[] = "43071";
 
 for (i = 0; srg[i] != '\0'; i++)
 {
-for (j = 0; lw[j] != '\0'; j++)
+
+for (j = 0; lw[j] != '\0'&& up[j] != '\0'; j++)
 {
 if (srg[i] == lw[j] || srg[i] == up[j])
-srg[i] = num[j]
+{
+srg[i] = num[j];
 break;
+}
 }
 }
 return (srg);
