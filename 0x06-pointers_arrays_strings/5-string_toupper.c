f0e4373cbe411943b41985c591c97590d9ee55d5
Brightlink01 <Brightlink01@gmail.com>
2022-04-21 07:00:08 -0700

    commiting the task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+ *string_toupper - changes the lower case to upper case
+ *@str: is pointer that points to the strings;
+ *Return: char type str
+ *
+ */
+char *string_toupper(char *str)
+{
+int i;
+for (i = 0; i != '\0'; i++)
+{
+if (!isupper(str[i]))
+str[i] = toupper(str[i]);
+}
+}
