054e75a4b7ffdb0904ade9b33d4eb7aecaccdfd9
Brightlink01 <Brightlink01@gmail.com>
2022-04-21 12:33:10 -0700

    having upper case is important for tas

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,4 +1,5 @@
 #include"main.h"
+#include <ctype.h>
 
 /**
  *cap_string - it capitalizes all words of a string
@@ -7,6 +8,19 @@
  */
 char *cap_string(char *str)
 {
-  char sy[] = ",;.!?(){  \"}";
+char sy[] = ",;.!?(){  \"}";
+int i, j, x;
 
+for (i = 0; str[i] != '\0'; i++)
+{
+for (j = 0; sy[j] != '\0'; j++)
+{
+if str[i] == sy[j]
+{
+i++;
+str[i] = toupper(str[i]);
+break;
+}
+}
+}
 }
