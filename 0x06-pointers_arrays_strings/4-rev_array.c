a24c697f46dcf3250536ac74a5b2466a9686f8a1
Brightlink01 <Brightlink01@gmail.com>
2022-04-21 04:50:16 -0700

    commiting the task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,17 @@
+#include"main.c"
+/**
+ *reverse _array - function that reverse strings
+ *@a: a pointer that holds string to be reversed
+ *@n: n is the number of array of element
+ *Return: void always true;
+ */
+void reverse_array(int *a, int n)
+{
+int *rev, j, i;
+i = n - 1;
+j = 0;
+while (i >= 0)
+{
+re[j] = a[i];
+}
+}
