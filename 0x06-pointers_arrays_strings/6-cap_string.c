bbad5e07196131ece87de1c354d5d4e20ca622b1
Brightlink01 <Brightlink01@gmail.com>
2022-04-21 13:12:32 -0700

    commiting message task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,17 +10,27 @@ char *cap_string(char *str)
 {
 char sy[] = ",;.!?(){  \"}";
 int i, j;
-
+int count;
 for (i = 0; str[i] != '\0'; i++)
 {
+if (str[0] > 96 && str[0] < 123)
+count = 1;
 for (j = 0; sy[j] != '\0'; j++)
 {
-if (str[i] == sy[j])
-{
-i++;
-str[i] = toupper(str[i]);
-break;
+if (sy[j] == str[i])
+count = 1;
 }
+if (count)
+{
+if (str[i] > 96 && str[i] < 123)
+{
+str[i] -= 32;
+count = 0;
+}
+else if (str[i] > 6 && str[i] < 91)
+count = 0;
+else if (str[i] > 47 && str[i] < 58)
+count = 0;
 }
 }
 return (str);
