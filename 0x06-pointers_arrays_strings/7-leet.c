2a0bc611937e4649d7a9abfd2706cc73aacbde98
Brightlink01 <Brightlink01@gmail.com>
2022-04-21 11:21:23 -0700

    commitng the task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include"main.h"
+
+/**
+ *leet - function replaces alphabet by numbers
+ *@srg: pointer used pass strings
+ *Return: chp;
+ */
+
+char *leet(char *srg)
+{
+int i, j;
+
+char lw[] = "aeotl";
+char up[] = "AEOTL";
+char num[] = "43071";
+
+for (i = 0; srg[i] != '\0'; i++)
+{
+for (j = 0; lw[j] != '\0'; j++)
+{
+if (srg[i] == lw[j] || srg[i] == up[j])
+srg[i] = num[j]
+break;
+}
+}
+return (srg);
+}
