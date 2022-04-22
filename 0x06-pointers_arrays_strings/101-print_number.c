789f1ba1cbac4ec8c8edc668bdfa0bac6a2b8413
Brightlink01 <Brightlink01@gmail.com>
2022-04-22 11:21:07 -0700

    commitng the majic number

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,26 @@
+#include"main.h"
+
+/**
+ *print_number - prints integral numbers using putchar
+ *@n: integral variable 
+ *Return: void success
+ *
+ */
+void(int n)
+{
+  int x, y, hold, neg;
+  if (n < 0)
+    neg = 1;
+
+  if (n >= 10000)
+    for (x = 0; x <= 5; x++)
+      {
+	y = n / 10000;
+	hold = n % 10000
+	if (n >= 1000)
+	  {
+	    y =n/1000
+	    hold = n % 1000;
+	  }
+      }
+}
