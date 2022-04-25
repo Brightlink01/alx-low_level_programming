278ed17b2fa6720a365e3da65993d4d854263d00
Brightlink01 <Brightlink01@gmail.com>
2022-04-25 13:53:50 -0700

    commiting the boring task

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,52 +1,32 @@
-#include"main.h"
+#include "main.h"
 
 /**
- *strstr - find the first sub string 
- *@haystack: locates sub strings
- *@needle: occurance of string 
+ * _strstr - locates a substring
+ * @haystack: string in which to check for needle
+ * @needle: substring to find in haystack
+ *
+ * Return: pointer to beginning of needle in haystack or NULL if no match
  */
-char *strstr(char *haystack, char *needle)
-
+char *_strstr(char *haystack, char *needle)
 {
-register char *a, *b;
-b = needle;
-if (*b == 0)
+unsigned int i = 0, j = 0;
+
+while (haystack[i])
 {
-
-return haystack;
-
-}
-
-for ( ; *haystack != 0; haystack += 1) {
-if (*haystack != *b)
+while (needle[j] && (haystack[i] == needle[0]))
 {
-
-continue;
-
-}
-
-a = haystack;
-
-while (1)
-{
-
-if (*b == 0)
-{
-
-return haystack;
-
-}
-
-if (*a++ != *b++)
-{
-
+if (haystack[i + j] == needle[j])
+j++;
+else
 break;
-
 }
+if (needle[j])
+{
+i++;
+j = 0;
 }
-
-b = needle;
-
+else
+return (haystack + i);
 }
-return NULL;
+return (0);
 }
