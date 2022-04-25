ccf704391c0cd1e5bc37803b12b89eb3585c29f5
Brightlink01 <Brightlink01@gmail.com>
2022-04-25 14:11:47 -0700

    commitng the capy at last

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,20 +1,20 @@
-#include"main.h"
+#include "main.h"
 
 /**
- *_memset - fill the n bytes address with value
- *@s: the pointer holds the value
- *@b: charecter type to be set to memory addres
- *@n: size of memory address
- *Return: s the string
+ * _memset - fills memory with a constant byte
+ * @s: memory space to be filled
+ * @b: byte to fill with
+ * @n: number of spaces to fill
+ *
+ * Return: pointer to memory area s
  */
-
 char *_memset(char *s, char b, unsigned int n)
 {
+unsigned int i;
 
-unsigned int i = 0;
-for (i = 0; i <= n; i++)
+for (i = 0; i < n; i++)
 {
-s[i] = b;
+*(s + i) = b;
 }
 return (s);
 }
