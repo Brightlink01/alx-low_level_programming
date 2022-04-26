0c53054fd8236cf088ddf2b952127f617e8b73da
Brightlink01 <Brightlink01@gmail.com>
2022-04-26 04:07:14 -0700

    commiting the task resubmission

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,15 +2,15 @@
 
 /**
 * main - entry point
-* Return: 1 Advanced taxs hello w 
+* Return: 1 Advanced taxs hello world
 */
 
 int main(void)
 {
 char *ch1 = "and that piece of art is useful";
-char *ch2 = " \" Dora Korpar, 2015-10-19";
+char *ch2 = "\" - Dora Korpar, 2015-10-19";
 int i;
- for (i = 0; ch1[i] != '\0'; i++)
+for (i = 0; ch1[i] != '\0'; i++)
 {
 putchar(ch1[i]);
 }
