a4a57d9eac45e56b18af8544daf13ac70c22fb54
Brightlink01 <Brightlink01@gmail.com>
2022-04-27 09:52:01 -0700

    comiiting the task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -8,19 +8,13 @@
  *
  */
 
-int int _pow_recursion(int x, int y)
+int _pow_recursion(int x, int y)
 {
 if (y < 0)
 return (-1);
-
-if (y == 0)
-return (1);
-
-if (y == 1)
-return (y);
 if(y >= 1)
 {
-y--;
 return (x * _pow_recursion(x, y - 1));
 }
+return (1);
 }
