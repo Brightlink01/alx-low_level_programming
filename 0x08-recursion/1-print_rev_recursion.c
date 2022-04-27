7698292bea87849941ca8b34e88b8e89681fb817
Brightlink01 <Brightlink01@gmail.com>
2022-04-27 08:06:52 -0700

    commiting the task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include"main.h"
+
+/**
+ *_print_rev_recursion - reverse the out put of string
+ *@s: s is the string to printed
+ *Return: void successful / correct
+ *
+ */
+
+void _print_rev_recursion(char *s)
+{
+if (*s == '\0')
+{
+return;
+_print_rev_recursion(s + 1);
+_putchar(*s);
+
+}
+}
