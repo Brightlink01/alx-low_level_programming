126202cef581cbbc80f2de692faee0e359f7af48
Brightlink01 <Brightlink01@gmail.com>
2022-04-27 15:54:26 -0700

    commiting the task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,17 +1,41 @@
-int prima(int n)
- {
-     int x = 1;
-     int y ;
-     if (n== 1|| n == 2)
-     return (1);
-     if (n == x * x)
-       return (x)
-  
-       
- }
- 
+#include"main.h"
+/**
+ *is_prime_number - chechks prime number
+ *@n: n is the number tobe checked
+ *Return: 1 for prime and 0 otherwise
+ *
+ */
+
+int is_prime_number(int n)
+{
+int x;
+int y;
+x = 2;
+
+if (n == 2)
+return (1);
+if (n <= 1)
+
+return (0);
+y = is_prime(n, x);
+return (y);
+
+}
+/**
+ *is_prime - calculate primes
+ *@n: number to passed
+ *@x: fatoril finder
+ *
+ *Return: the prime 1 for prime
+ */
+
 int is_prime(n, x)
 {
-  if (n == x * x)
-    return (0);
-  
+
+if (n % x == 0)
+return (0);
+if (x * x > n)
+return (1);
+return (is_prime(n, x + 1));
+
+}
