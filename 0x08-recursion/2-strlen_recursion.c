a5a79eb49c189001e28eb22a8abe68c9c9c0fbe4
Brightlink01 <Brightlink01@gmail.com>
2022-04-27 08:32:36 -0700

    commiting the task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,25 @@
+#include"main.h"
+
+/**
+ *_strlen_recursion - retrn s the lenth of strings Tsedal
+ *@S: string the are found tsedal hold void pointer of fun
+ *Return: int len is the string found in me
+ *
+ */
+
+int _strlen_recursion(char *s)
+{
+
+int len;
+len = 0;
+if (*s == '\0')
+return (1);
+else
+{
+
+len++;
+_strlen_recursion(s + 1);
+
+return (len);
+
+}
