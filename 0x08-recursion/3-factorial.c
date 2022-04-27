20975061c56260394e69c1d51870d4c4b837eae4
Brightlink01 <Brightlink01@gmail.com>
2022-04-27 09:11:12 -0700

    commiting the task 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include"main.h"
+
+/**
+ *factorial - return the factoria of n
+ *@n: n is the integral number to be factored
+ *
+ *Return: int the fact of n;
+ *
+ */
+
+int factorial(int n)
+{
+if (n <= 1)
+{
+
+return (1);
+}
+return (n * factorial(n - 1));
+}
