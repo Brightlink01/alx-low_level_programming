807ea4bdf95da97d85007d6fb7277d36327f4c6f
Brightlink01 <Brightlink01@gmail.com>
2022-04-27 08:59:10 -0700

    commiting the task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -12,14 +12,10 @@ int _strlen_recursion(char *s)
 
 int len;
 len = 0;
+
 if (*s == '\0')
-return (1);
-else
-{
-
+return (0);
 len++;
-_strlen_recursion(s + 1);
-
+len = len + _strlen_recursion(s + 1);
 return (len);
-}
 }
