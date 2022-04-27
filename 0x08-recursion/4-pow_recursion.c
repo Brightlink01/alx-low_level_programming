1ad0c3a565999431a993e672b628df8f4efa95b3
Brightlink01 <Brightlink01@gmail.com>
2022-04-27 09:43:27 -0700

    commiting the function sqrt

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,26 @@
+#include"main.h"
+
+/**
+ *_pow_recursion - the recursion power fun
+ *@x:is the powering int
+ *@y: is the powered int
+ *Return: power of x
+ *
+ */
+
+int int _pow_recursion(int x, int y)
+{
+if (y < 0)
+return (-1);
+
+if (y == 0)
+return (1);
+
+if (y == 1)
+return (y);
+if(y >= 1)
+{
+y--;
+return (x * _pow_recursion(x));
+}
+}
