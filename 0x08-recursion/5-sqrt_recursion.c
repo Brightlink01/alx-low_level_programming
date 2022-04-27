d20bffc9acba87dd74d77027958610f41059d7fa
Brightlink01 <Brightlink01@gmail.com>
2022-04-27 14:41:08 -0700

    commiting the task partial

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,7 +1,7 @@
 #include"main.h"
 
 /**
- *_sqrt_recursion - some of us squere the problem 
+ *_sqrt_recursion - some of us squere the problem
  *@n: is the rout of squere problems so as to minimize
  *Return: the of a rout of the broble and solved esle -1
  *
@@ -16,10 +16,15 @@ return (1);
 y = is_natural(n, x);
 return (y);
 }
+/**
+ *is_natural - calculates natural value
+ *@n: number to be squere routed
+ *@x: factor finder
+ *Return: the squere rout
+ */
 int is_natural(int n, int x)
 {
 if (x * x == n)
 return (x);
- 
 return (n % is_natural(n, n - 1));
 }
