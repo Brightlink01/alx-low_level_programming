63f0583778202e8c48b0ae64d181f111705beaea
Brightlink01 <Brightlink01@gmail.com>
2022-04-28 12:04:17 -0700

    commitig the tas 101

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,21 +1,28 @@
 #include"main.h"
 
 /**
- *wildcmp - comparing the strings 
+ *wildcmp - comparing the strings
  *@s1: the first string to be compare
  *@s2: the second string to be compared
- *Return: integral value 
+ *Return: integral value
  *
  */
 
 int wildcmp(char *s1, char *s2)
 {
 
-if(*s1 != *S2)
+int err;
+err = 0;
+
+if (strlen(s1) - strlen(s2) > 1 || strlen(s1) - strlen(s2) < 1)
 return (0);
 
-if (*s1 == *s2)
-return (s1 + 1, s2 + 1);
+if (*s1 == '\0' && *s2 == '\0' || err == 2)
+return (err);
 
-return (1);
+if (*s1 != *s2)
+err++;
+
+return (wildcmp(s1 + 1, s2 + 1));
+
 }
