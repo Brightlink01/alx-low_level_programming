0b31af866ee4aafe0952ca4ed1fbd36989eb445d
Brightlink01 <Brightlink01@gmail.com>
2022-04-28 12:14:33 -0700

    comting the task 101

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -18,10 +18,10 @@ err = 0;
 if (strlen(s1) - strlen(s2) > 1 || strlen(s1) - strlen(s2) < 1)
 return (0);
 
- if ((*s1 == '\0' && *s2 == '\0') || err == 2)
+if ((*s1 == '\0' && *s2 == '\0') || err == 2)
 return (err);
 
-if (*s1 != *s2)
+if (*s1 != *s2 || *s1 != '*' || *s2 != '*')
 err++;
 
 return (wildcmp(s1 + 1, s2 + 1));
