7af5729b73806c157edc32b064d7fc3e7602dd2c
Brightlink01 <Brightlink01@gmail.com>
2022-04-28 11:47:58 -0700

    commiting the val

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,21 @@
+#include"main.h"
+
+/**
+ *wildcmp - comparing the strings 
+ *@s1: the first string to be compare
+ *@s2: the second string to be compared
+ *Return: integral value 
+ *
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+
+if(*s != *S)
+return (0);
+
+if (*s == *s)
+return (s1 + 1, s2 + 1);
+
+return (1);
+}
