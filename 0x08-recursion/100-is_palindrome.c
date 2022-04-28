b2e85c6a97e831caa750e01dff6f78fe60c95644
Brightlink01 <Brightlink01@gmail.com>
2022-04-28 11:39:01 -0700

    commiting the tas 100

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -2,7 +2,7 @@
 #include <string.h>
 /**
  *is_palindrome - check for palidrome
- *@S: the string to be checked
+ *@s: the string to be checked
  *Return: Integral value 1 or 0
  *
  */
@@ -12,16 +12,16 @@ int is_palindrome(char *s)
 
 int n = strlen(s);
 if (n == 0)
-return 1;
+return (1);
 
-return isPalRec(s, 0, n - 1);
+return (isPalRec(s, 0, n - 1));
 }
 
 /**
  *isPalRec - compares the string back to fromt
  *@s: is stream of string
  *@e: the size of the strings
- *@str: the stream of the string 
+ *@str: the stream of the string
  *Return: integral value
  */
 int isPalRec(char *str, int s, int e)
@@ -32,7 +32,7 @@ return (1);
 if (str[s] != str[e])
 return (0);
 if (s < e + 1)
-return isPalRec(str, s + 1, e - 1);
+return (isPalRec(str, s + 1, e - 1));
 return (1);
 
 }
