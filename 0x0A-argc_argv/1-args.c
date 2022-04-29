7480502b5e4baa8dccf79f1b0d6e6d14d7ea5111
Brightlink01 <Brightlink01@gmail.com>
2022-04-29 13:06:54 -0700

    commint tasx 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - main function
+ *@argc: integral part that count the argument
+ *@argv: an array that takes the argument
+ *Return: Always 0, successful
+ *
+ */
+int main(int argc, char *argv[])
+{
+int i;
+for (i = 0; i < argc; i++)
+{
+printf("%s ", argv[i]);
+}
+printf("\n");
+return (0);
+
+}
