2f5e8a8f55b13914ce6a7d22cf4c671496a49ebf
Brightlink01 <Brightlink01@gmail.com>
2022-04-29 13:34:05 -0700

    commitng the task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
  *main - main function
  *@argc: integral part that count the argument
@@ -8,7 +8,7 @@
  *
  */
 
-int main(void argc, char *argv[])
+int main(int argc, char *argv[])
 {
 int i;
 for (i = 0; i < argc; i++)
