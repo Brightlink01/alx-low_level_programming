83cd6fb8e0163857cfe08cfd0001916d6d0b4cdd
Brightlink01 <Brightlink01@gmail.com>
2022-04-29 15:19:30 -0700

    task luv

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include<ctype.h>
+#include<stdlib.h>
+/**
+ *main - main function
+ *@argc: integral part that count the argument
+ *@argv: an array that takes the argument
+ *Return: Always 0, successful
+ *
+ */
+int main(int argc, char *argv[])
+{
+int i, sum;
+sum = 0;
+if (argc == 1)
+{
+
+printf("%d\n", 0);
+}
+    
+for (i = 1; i < argc; i++)
+{
+
+if (!isdigit(argv[i]))
+{
+printf("Error\n");
+return (1);
+break;
+}
+sum += atoi(argv[i]); 
+}
+
+printf("%d\n", sum);
+return (0);
+
+}
