51297663c05eb82fca2d49da919f6357b02fe6ec
Brightlink01 <Brightlink01@gmail.com>
2022-04-30 10:16:13 -0700

    commit task1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -11,16 +11,9 @@
 
 int main(int argc, char *argv[])
 {
-int i;
-int sum;
-sum = 0;
-if (argc > 0)
-for (i = 0; i < argc; i++)
-{
-sum += atoi(argv[i]);
-}
 
-printf("%d \n", sum);
+if (!argv[argc])
+printf("%d \n", argc - 1);
 return (0);
 
 }
