60dee986483f807a33c2c8eb71828a8de09ae4e5
Brightlink01 <Brightlink01@gmail.com>
2022-04-30 09:33:48 -0700

    committing the message

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,17 +2,18 @@
 #include <stdlib.h>
 
 /**
- *main - main function 
- *@argc: integral part that count the argument 
- *@argv: an array that takes the argument 
+ *main - main function
+ *@argc: integral part that count the argument
+ *@argv: an array that takes the argument
  *Return: Always 0, successful
  *
  */
 int main(int argc, char *argv[])
 {
-
+int i;
+{
 if (argc >= 0)
-printf("%s\n", argv[0]);
+printf("%s\n", argv[argc]);
 return (0);
-
+}
 }
