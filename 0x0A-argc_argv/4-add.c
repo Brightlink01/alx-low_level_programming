4eac7b158f76adce744432c745ea5cc3aeb4e62f
Brightlink01 <Brightlink01@gmail.com>
2022-05-01 12:53:19 -0700

    commiting the task 100

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,24 +9,43 @@
  *Return: Always 0, successful
  *
  */
-
+boolean checkdigit(int n)
+{
+if (isdigit(n))
+return (true);
+else
+return (false);
+}
+    
 int main(int argc, char *argv[])
 {
 int i, sum;
+boolean bol;
 sum = 0;
 if (argc <= 1)
 {
 printf("%d\n", 0);
 }
 else
-{ 
-for (i = 1; i < argc; i++)
 {
-sum += atoi(argv[i]);
-}
 
+while (i < argc)
+{
+
+bol = checkdigit(argv[i]);
+if (bol)
+sum += atoi(argv[i]);
+else
+break;
+}
+if (!bol)
+{
+printf("Error\n")
+return (1);
+}
 printf("%d\n", sum);
 }
+
 return (0);
 
 }
