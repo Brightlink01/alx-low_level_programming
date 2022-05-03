dd855824025ad7b45966e48c75425189c5dccac2
Brightlink01 <Brightlink01@gmail.com>
2022-05-03 10:18:28 -0700

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -26,12 +26,12 @@ if (con_str == NULL)
 {
 return (NULL);
 }
-if (s1[0] != '\0')
+if (s1 == NULL)
 for (i = 0; s1[i] != '\0'; i++)
 {
 con_str[i] = s1[i];
 }
-if (s2[0] != '\0')
+if (s2 == NULL)
 while (s2[j] != '\0')
 {
 con_str[i] = s2[j];
