0a37658f653de79f42242717beeefcec551e5cb2
Brightlink01 <Brightlink01@gmail.com>
2022-05-03 07:38:03 -0700

    task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,40 @@
+#include"main.h"
+#include<string.h>
+
+/**
+ *str_concat - conacatinates strings 
+ *@s1: s1 string to be concatenaned 
+ *@s2: s2 string to bee concatenated
+ *Return: char type pointer 
+ *
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+char *con_str
+unsigned int len, len1, len2, i, j;
+
+len1 = strlen(s1);
+len2 = strlen(s2);
+len = len1 + len2;
+j = 0; 
+if (len1 == 0 || len2)
+con_str = " ";
+con_str = malloc((len + 1) * sizeof(char));
+if (con_str == NULL)
+{
+return (NULL);
+}
+for (i = 0; i <= len1; i++)
+{
+con_str[i] = s1[1];
+}
+while (j <= len2)
+{
+con_str[i] == s2[j];
+i++;
+j++;
+}
+
+return (con_str);
+}
