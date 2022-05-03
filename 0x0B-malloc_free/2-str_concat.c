ceeee9de0aadb1bdacf4553edbadb68097e5930b
Brightlink01 <Brightlink01@gmail.com>
2022-05-03 07:54:57 -0700

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -20,7 +20,7 @@ len1 = strlen(s1);
 len2 = strlen(s2);
 len = len1 + len2;
 j = 0;
-if (s1 == '\0' && s2 == '\0')
+if (*s1 == '\0' && *s2 == '\0')
 con_str = " ";
 con_str = malloc((len + 1) * sizeof(char));
 if (con_str == NULL)
