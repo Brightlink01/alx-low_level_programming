5a518d90709c7a4068ab5af0d94aedd94df4d7de
Brightlink01 <Brightlink01@gmail.com>
2022-05-12 12:49:15 -0700

    commit task send

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,25 +1,22 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * free_grid - function to free memory
- * @grid: pointer to grid of int type
- * @height: int type
- * Return: clean up memory
+ * free_grid - Free a the two dimensional grid
+ *
+ * @grid: The free up the grids
+ * @height: The number of rows in the grid
+ *
+ * Return: Nothing
  */
 
 void free_grid(int **grid, int height)
 {
-	int y;
+	int i = 0;
 
-	if (grid == NULL || grid == 0)
+	while (i < height)
 	{
-		return;
-	}
-	for (y = 0; y < height;  y++)
-	{
-		free(grid[y]);
+		free(grid[i]);
+		i++;
 	}
 	free(grid);
 }
