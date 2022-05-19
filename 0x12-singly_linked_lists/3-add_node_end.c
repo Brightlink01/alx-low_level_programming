3e3b8ad8d0a41b8fc7de03a179571cb1568d21f5
Brightlink01 <Brightlink01@gmail.com>
2022-05-19 10:04:50 -0700

    commiting the tasks

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,51 @@
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+  * *add_node_end - adds a new node at the end of a linked list.
+  * @head: start of the list.
+  * @str: string.
+  * Return: the address of the new element,
+  * or null if it failed.
+  */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *newNode;
+
+	newNode = (list_t *)malloc(sizeof(list_t));
+	if (newNode == NULL)
+		return (NULL);
+	newNode->str = strdup(str);
+	newNode->len = string_length(str);
+	newNode->next = NULL;
+	if (*head == NULL)
+		*head = newNode;
+	else
+	{
+		list_t *lastNode = *head;
+		while (lastNode->next != NULL)
+		{
+			lastNode = lastNode->next;
+		}
+
+		lastNode->next = newNode;
+	}
+	return (newNode);
+}
+
+/**
+  * string_length - finds the length of a string.
+  * Return: length of c.
+  * @pointer: pointer.
+  */
+int string_length(const char *pointer)
+{
+	int c = 0;
+
+	while (*(pointer + c) != '\0')
+	{
+		c++;
+	}
+	return (c);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,13 @@
+- How to build and use linked lists
+0. Print list
+1. List length
+2. Add node
+3. Add node at the end
+4. Free list
+5. The Hare and the Tortoise
+6. Real programmers can write assembly code in any language
+
+************************ This project is ALX  SE program
+************************ To fullfill the low level prog
+************************ 0x12c The linked list
+********************* done by Tsedal Tsegaye 2022
