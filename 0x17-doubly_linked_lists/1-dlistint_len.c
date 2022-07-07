265b2c3d3979ec3b18196797a9e314b8a1ec7f63
Brightlink01 <Tsedal Lombebo>
2022-07-07 19:11:53 +0000

    fixin the errors

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -8,13 +8,11 @@
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-  size_t nodes = 0;
-
-  while (h)
-    {
-      nodes++;
-      h = h->next;
-    }
-
-  return (nodes);
+size_t nodes = 0;
+while (h)
+{
+nodes++;
+h = h->next;
+}
+return (nodes);
 }
--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,8 +1,3 @@
-/*
- * File: 4-free_dlistint.c
- * Auth: Brennan D Baraban
- */
-
 #include "lists.h"
 
 /**
@@ -11,12 +6,12 @@
  */
 void free_dlistint(dlistint_t *head)
 {
-  dlistint_t *tmp;
+dlistint_t *tmp;
 
-  while (head)
-    {
-      tmp = head->next;
-      free(head);
-      head = tmp;
-    }
+while (head)
+{
+tmp = head->next;
+free(head);
+head = tmp;
+}
 }
